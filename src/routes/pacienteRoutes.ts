baf308770af10f357d34bf6bfb42e7c2f416c92e
Vianaat <vianat442@gmail.com>
2024-09-18 15:49:19 -0300

    fix:remove file validation

--- src/routes/pacienteRoutes.ts
+++ src/routes/pacienteRoutes.ts
@@ -6,8 +6,4 @@ const pacienteRoutes = express.Router();
 
 pacienteRoutes.post("/paciente",CreatePacienteController.createPaciente)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> dce18613f255231574efc05a4120fe34e629cbb7
 export default pacienteRoutes;
