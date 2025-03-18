8b68039e6315bcc31cd51d652ffd3f46563e48e4
leticiasbarros23 <leticiabarrosbferreira@gmail.com>
2025-03-17 21:00:50 -0300

    corrigindo

--- src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-// Conectar ao Supabase
-const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
-
-export default supabase;
--- src/middlewares/upload.ts
+++
@@ -1,6 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage(); // Mantém a imagem na RAM antes do envio
-const upload = multer({ storage });
-
-export default upload;
