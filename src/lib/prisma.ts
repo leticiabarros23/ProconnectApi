8e06a28bdbad41456aa53ebe980077b00608b081
leticiasbarros23 <leticiabarrosbferreira@gmail.com>
2026-02-25 00:35:16 -0300

    fix prisma client import

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from "@prisma/client/extension";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
+
+// @ts-ignore
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const prisma = new PrismaClient({ adapter });
